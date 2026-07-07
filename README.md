8d7701c4e13471852b90a68f6b4cdfe433b339cb
Bon_26 <bonnie22556l@gmail.com>
2026-07-07 20:52:14 +0700

    Auto commit 07.07.2026 20:52:14,17

---
+++ README.md
@@ -0,0 +1,2 @@
+# fyafs
+five years at furry shodka
---
+++ autopush.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "D:\ATLauncher\instances\Minecraft12111fyafs\resourcepacks\tp"
+git add .
+git commit -m "Auto commit %date% %time%"
+git push origin main
